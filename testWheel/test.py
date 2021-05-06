daece1047ea22e6083f49e0738687e183995945f
Mike Belov <47603679+belovm96@users.noreply.github.com>
2021-05-06 15:51:04 -0400

    added py files

---
+++ testWheel/test.py
@@ -0,0 +1,7 @@
+from testWheel.__init__ import * 
+def func_test(): 
+    print("Successfully Imported test.py file") 
+def print_name(name): 
+    print(f'Hello {name}') 
+def print_test_age(age): 
+    print_age(age)
